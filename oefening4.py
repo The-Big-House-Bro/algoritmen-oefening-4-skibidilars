ba0ac121a5cefe1df18ac434c35330b5e3b9f0ee
Lars De Proft <lars.deproft@gmail.com>
2025-03-27 18:29:18 +0100

    duurde lang

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,14 @@
-def countTargetPairs():
+nums = [-1,1,2,3,1]
+target = 2
+
+def countTargetPairs(nums, target):
+    antwoord = 0
+    n = len(nums)
+    
+    for i in range(n):
+        for j in range(i + 1, n):  
+            if nums[i] + nums[j] < target:
+                antwoord += 1
+    
+    print(antwoord)
+countTargetPairs(nums, target)
